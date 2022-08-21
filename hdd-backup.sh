813cbcecd9723967f75535942a96bbf256f9483c
Antoine Lafarge <ant.lafarge@gmail.com>
2022-08-21 04:46:53 +0200

    Update hdd-backup.sh

--- hdd-backup.sh
+++ hdd-backup.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 # Usage :
-# sudo ./backup-hdd.sh /dev/sdX "/mnt/hdd/Backups/backup 1"
+# sudo ./backup-hdd.sh /dev/from /dev/to
 
-DEV=$1
-TMP="/mnt/TMP_$(cat /proc/sys/kernel/random/uuid)"
-DEST=$2
+sourceDevice=$1
+sourcePath="/mnt/TMP_$(cat /proc/sys/kernel/random/uuid)"
+destDevice=$2
+destPath="/mnt/TMP_$(cat /proc/sys/kernel/random/uuid)"
 
 echo "Backup files"
-echo "Source device : $DEV"
-echo "Temp directory : $TMP"
-echo "Target directory : $DEST"
+echo "Source device : $sourceDevice"
+echo "Source temp directory : $sourcePath"
+echo "Destination device : $destDevice"
+echo "Destination temp directory : $destPath"
 
-sudo mkdir -p "$TMP"
-sudo mount -o ro "$DEV" "$TMP"
-sudo mkdir -p "$DEST"
-sudo rsync -avhP "$TMP/" "$DEST"
-sudo umount "$DEV"
-sudo rm -rf "$TMP"
+sudo mkdir "$sourcePath"
+sudo mkdir "$destPath"
+sudo mount -o ro "$sourceDevice" "$sourcePath"
+sudo mount -o rw "$destDevice" "$destPath"
+sudo rsync -avhP "$sourcePath/" "$destPath/"
+sudo umount "$destDevice"
+sudo umount "$sourceDevice"
+if [ $? -eq "0" ]
+then
+	sudo rm -rf "$sourcePath"
+	sudo rm -rf "$destPath"
+fi
 
 echo "Backup finished!"
