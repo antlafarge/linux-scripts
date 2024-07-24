eb2e2e519491b572a210635077d158e23b41d78e
Antoine Lafarge <ant.lafarge@gmail.com>
2024-07-25 00:29:27 +0200

    Update duckdns.sh

--- duckdns.sh
+++ duckdns.sh
@@ -2,6 +2,8 @@
 
 # Usage :
 #     ./duckdns.sh "myDomain" "myToken"
+# Example for "example.duckdns.org" :
+#     ./duckdns.sh "example" "12345678-1234-1234-1234-123456789ABC"
 # crontab :
 #     crontab -e
 # Add this line to execute the script (every day) :
@@ -20,20 +22,17 @@ ipV6=$(curl -s "https://api64.ipify.org/?format=json" | sed -r "s/${regexPattern
 
 duckDnsUrl="https://www.duckdns.org/update?domains=${domain}&token=${token}&verbose=true"
 
-if [ -n "${ipV4}" ]
-then
+if [ -n "${ipV4}" ]; then
     duckDnsUrl="${duckDnsUrl}&ip=${ipV4}"
 fi
 
-if [ -n "${ipV6}" ]
-then
+if [ -n "${ipV6}" ] && [ "${ipV4}" != "${ipV6}" ]; then
     duckDnsUrl="${duckDnsUrl}&ipv6=${ipV6}"
 fi
 
 res=$(curl -s "${duckDnsUrl}")
 
-if [ "$res" = "KO" ]
-then
+if [ "$res" = "KO" ]; then
     echo "Update duckdns.org failed"
     echo "duckDnsUrl=${duckDnsUrl}"
     echo "result=${res}"
